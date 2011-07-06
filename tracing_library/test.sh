0506a3622287758f259502f052898213b3d0ad9d
Vladimir Subotic <vladimir@vladimir-Latitude-E6410.(none)>
2011-07-06 20:46:26 +0200

    local machine working with sequential programs

--- tracing_library/test.sh
+++ tracing_library/test.sh
@@ -16,13 +16,12 @@ diff output.test_dep_calculator default.test_dep_calculator
 echo '------------------------------ending test_taskname_coder'
 
 
-export MPTRACE_CONFIG_FILE=./mpitrace.xml
-export LD_PRELOAD=/home/vladimir/work/mpitrace-install/lib/
-# /gpfs/scratch/bsc18/bsc18422/mpitrace/extrae-2.0-linux-ppc64-mpichmx-papi-3.6.2/lib/libmpitrace.so
-export MPITRACE_ON=1
-
-echo '----------------------------starting mpi_probe'
-mpirun -n 1 mpi_probe > output.mpi_probe
-diff output.mpi_probe default.mpi_probe
-echo '------------------------------ending test_taskname_coder'
+# export MPTRACE_CONFIG_FILE=./mpitrace.xml
+# export LD_PRELOAD=/home/vladimir/work/mpitrace-install/lib/
+# export MPITRACE_ON=1
+# 
+# echo '----------------------------starting mpi_probe'
+# mpirun -n 1 mpi_probe > output.mpi_probe
+# diff output.mpi_probe default.mpi_probe
+# echo '------------------------------ending test_taskname_coder'
 
