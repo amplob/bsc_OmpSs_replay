3ad0342a6262576351313b4fc5721229313fd02c
bsc <amplob@gmail.com>
2015-09-21 16:19:19 +0200

    deleted unnecessary file and unnecessary calls to extrae in simple_test

--- tests/simple_test/simple_test.c
+++ tests/simple_test/simple_test.c
@@ -23,47 +23,37 @@ int main(int argc, char *argv[])
 
 #pragma omp task out(a)
       {
-	Extrae_event( 1000, 1);
-// 	  printf("starting task a\n");
           some_work(X, 'a');
-// 	  printf("ending task a\n");
-	  Extrae_event( 1000, 0);
       }
+      
 #pragma omp task in(a) out(b)
       {
-	Extrae_event( 1000, 2);
           some_work(X, 'b');
-	  Extrae_event( 1000, 0);
       }
+      
 #pragma omp task in(a) out (c)
       {
-	Extrae_event( 1000, 3);
           some_work(X, 'c');
-	  Extrae_event( 1000, 0);
       }    
+      
 #pragma omp task in(c) out (d)
       {
-	Extrae_event( 1000, 4);
           some_work(X, 'd');
-	  Extrae_event( 1000, 0);
       }
+      
 #pragma omp task in(d) out (e)
       {
-	Extrae_event( 1000, 5);
           some_work(X, 'e');
-	  Extrae_event( 1000, 0);
       }
+      
 #pragma omp task in(d) out (f)
       {
-	Extrae_event( 1000, 6);
           some_work(X, 'f');
-	  Extrae_event( 1000, 0);
       }
+      
 #pragma omp task in(b) in (e) in (f)
       {
-	Extrae_event( 1000, 7);
           some_work(X, 'g');
-	  Extrae_event( 1000, 0);
       }
 
       Extrae_fini();
