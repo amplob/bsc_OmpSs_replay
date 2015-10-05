8f2230b2a9adef7660037bb2de2ae6c4339c8e47
bsc <amplob@gmail.com>
2015-10-05 16:55:25 +0200

    deleted unnecessary files, simple test is now test_inout

--- tests/simple_test/exe.sh
+++
@@ -1,6 +0,0 @@
-export EXTRAE_HOME=/home/bsc/bsc/tools/extrae-install
-export EXTRAE_CONFIG_FILE=./extrae.xml
-export LD_PRELOAD=${EXTRAE_HOME}/lib/libseqtrace.so:/home/bsc/bsc/tools/OmpSs_replay/install/lib/libmpissTrace.so
-export EXECUTABLE=executable.exe # This is taken by extrae.xml
-
-./executable.exe
--- tests/test_inout/test_inout.c
+++ tests/test_inout/test_inout.c
@@ -6,7 +6,7 @@ void some_work(int *X, char task) {
 	int i;
 	for (i=0; i<100000; ++i) X[i]=i;
 	
-	printf("Thread %d have done task %c\n", omp_get_thread_num(), task);
+	//printf("Thread %d have done task %c\n", omp_get_thread_num(), task);
 	
 }
 
@@ -21,41 +21,40 @@ int main(int argc, char *argv[])
       printf("starting program\n");
       Extrae_init();
 
-#pragma omp task out(a)
+#pragma omp task inout(X)
       {
           some_work(X, 'a');
       }
       
-#pragma omp task in(a) out(b)
+#pragma omp task inout(X)
       {
           some_work(X, 'b');
       }
       
-#pragma omp task in(a) out (c)
+#pragma omp task inout(X)
       {
           some_work(X, 'c');
       }    
       
-#pragma omp task in(c) out (d)
+#pragma omp task inout(X)
       {
           some_work(X, 'd');
       }
       
-#pragma omp task in(d) out (e)
+#pragma omp task inout(X)
       {
           some_work(X, 'e');
       }
       
-#pragma omp task in(d) out (f)
+#pragma omp task inout(X)
       {
           some_work(X, 'f');
       }
       
-#pragma omp task in(b) in (e) in (f)
+#pragma omp task inout(X)
       {
           some_work(X, 'g');
       }
-
       Extrae_fini();
 #pragma omp taskwait
       return 0;
