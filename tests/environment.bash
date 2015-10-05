9af3c67fd495c6fbbdec26f6d0bafbc9d2cc26ec
bsc <amplob@gmail.com>
2015-10-05 19:01:38 +0200

    bash needed to generate traces

--- tests/environment.bash
+++ tests/environment.bash
@@ -1,4 +1,11 @@
-export EXTRAE_HOME=/home/bsc/bsc/tools/extrae-install
-# export PARAVER_HOME=/home/marc/tools/wxparaver-4.5.8-linux_fc6-x86_64
-# export PATH=$PARAVER_HOME/bin:$PATH
-# 
+
+export TOOLS_HOME=/home/bsc/bsc/tools
+
+export PATH=$TOOLS_HOME/dimemas-5.2.9-linux-x86_64/bin:$PATH
+export PATH=$TOOLS_HOME/trf2trf_install/bin:$PATH
+export PATH=$TOOLS_HOME/scripts/dimemas_simulations:$PATH
+export EXTRAE_HOME=$TOOLS_HOME/extrae-install
+
+export PARAVER_HOME=$TOOLS_HOME/wxparaver-4.5.8-linux_fc6-x86_64
+export PATH=$PARAVER_HOME/bin:$PATH
+
