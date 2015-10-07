2fa7642b9def1c7a6d29f72b2a1a1f26d255876a
bsc <amplob@gmail.com>
2015-10-07 19:51:38 +0200

    Deleted some debugging lines

--- tests/exe.sh
+++ tests/exe.sh
@@ -1,10 +1,6 @@
-pwd
-echo this is the param i get
-echo $1
 cd $1
-pwd
-# export EXTRAE_HOME=/home/bsc/bsc/tools/extrae-install
-# export EXTRAE_CONFIG_FILE=../extrae.xml
-# export LD_PRELOAD=${EXTRAE_HOME}/lib/libseqtrace.so:/home/bsc/bsc/tools/OmpSs_replay/install/lib/libmpissTrace.so
-# ./executable.exe
+export EXTRAE_HOME=/home/bsc/bsc/tools/extrae-install
+export EXTRAE_CONFIG_FILE=../extrae.xml
+export LD_PRELOAD=${EXTRAE_HOME}/lib/libseqtrace.so:/home/bsc/bsc/tools/OmpSs_replay/install/lib/libmpissTrace.so
+./executable.exe
 cd ..
