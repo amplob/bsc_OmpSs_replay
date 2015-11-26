cc909e3a323da9e0ba3f0bfd2bb45891152b9a9d
bsc <amplob@gmail.com>
2015-11-26 16:16:00 +0100

    test_concurrent1 added

---
+++ tracing_library/tests/test_concurrent1/test_concurrent1.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+
+#define SIZE1 10000
+#define SIZE2 SIZE1*10
+
+#pragma omp task inout(X[0;10000]) no_copy_deps
+void some_work_inout(int *X) {
+	int i;
+	for (i=0; i<SIZE2; ++i) X[i]+=i;
+	printf("finish some_work_inout\n");
+}
+
+#pragma omp task concurrent (X[0;10000]) no_copy_deps
+void some_work_conc(int *X, int part) {
+	int i;
+	for (i=0; i<SIZE1; ++i) 
+	    X[part * SIZE1 + i]+=i;
+	printf("finish some_work_concurrent, values from %d to %d\n", part * SIZE1, part * SIZE1 +999);
+}
+
+
+
+int main(int argc, char *argv[])
+{
+      int X[SIZE2];
+      int Y[SIZE2];
+      
+      int i;
+      char *task_name = "task number 1";
+      
+      printf("starting program\n");
+      //Extrae_init();
+      
+      for (i=0; i<2; i++) 
+      {
+	some_work_conc(X, i);
+      }
+
+      for (i=0; i<2; i++) 
+      {
+	some_work_conc(Y, i);
+      }
+      #pragma omp taskwait
+      some_work_inout(Y);
+
+      for (i=0; i<2; i++) 
+      {
+	some_work_conc(X, i);
+      }
+      #pragma omp taskwait
+
+      some_work_inout(X); 
+      
+      printf("program end\n");
+      #pragma omp taskwait
+      
+      return 0;
+      //Extrae_fini();
+      //return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
